9ca355a8763caf6b2e4874e8e64ad8f5b9a2bf52
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-14 13:31:08 +0900

    개별 식사 항목 추가 및 표시

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
-import classes from './AvailableMeals.module.css';
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -28,11 +30,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map(meal => <li>{ }</li>);
+  const mealList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    ></MealItem>
+  ));
 
   return (
-    <section>
-      <ul>{mealList}</ul>
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
   );
 };
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,1 +1,14 @@
-const Meas=
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary></MealsSummary>
+      <AvailableMeals></AvailableMeals>
+    </Fragment>
+  );
+};
+
+export default Meals;
